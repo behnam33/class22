3fd597c3b4bb5e44a5445992f1cd757f515878d3
behnam33 <behnam.ghezelbash.7@gmail.com>
2019-04-19 18:28:35 +0330

    odd

--- start4.py
+++ start4.py
@@ -1,5 +1,5 @@
-list = [20,30,40,50,60,70,80,1,2,3,4,5,6,7]
-list1=sum(list[0:8])
+list = [20,30,40,50,60,70,80,90,1,2,3,4,5,6,7,8]
+list1=sum(list[0:])
 list2=sum(list[8:])
 list3=len(list[0:8])
 list4=len(list[8:])
