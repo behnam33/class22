c555c3b30f1675abc5aed9fa479e74b111b6e4f2
behnam33 <behnam.ghezelbash.7@gmail.com>
2019-04-19 19:21:44 +0330

    elif

---
+++ input.py
@@ -0,0 +1,11 @@
+value= input('enter number')
+if  value.isdigit():
+    print('cannot convert to numbers')
+else:
+    num= int (value)
+    if num % 3 == 0:
+      print('3k')
+    elif num % 3==1:
+        print('3k+1')
+    else:
+        print('3k+2')
